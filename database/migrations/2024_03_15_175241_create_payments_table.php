15739c18145442fc5d0c3bd4892ca9d112b2dab8
potatojoooooo <joanne0611@1utar.my>
2024-03-16 03:43:51 +0800

    first commit

---
+++ database/migrations/2024_03_15_175241_create_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->decimal('amount', 10, 2);
+            $table->string('status', 50);
+            $table->foreignIdFor(User::class, 'created_by');
+            $table->foreignIdFor(User::class, 'updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
